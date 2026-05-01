7cdcfe0767359dca77b7ea1d2b000746b4d08aa9
aline <alinedofini@gmail.com>
2026-05-01 18:55:40 +0200

    tarefa 3

--- main.py
+++ main.py
@@ -1,2 +1,53 @@
+import data.py
+
 
 class TestUrbanRoutes:
+    def test_set_route(self):
+        # Adicionar em S8
+
+        print("função criada para definir a rota")
+        pass
+
+    def test_select_plan(self):
+        # Adicionar em S8
+
+        print("função criada para selecionar plano")
+        pass
+
+    def test_fill_phone_number(self):
+        # Adicionar em S8
+
+        print("função criada para preencher telefone")
+        pass
+
+    def test_fill_card(self):
+        # Adicionar em S8
+
+        print("função criada para preencher cartão")
+        pass
+
+    def test_comment_for_driver(self):
+        # Adicionar em S8
+
+        print("função criada para avaliar motorista")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Adicionar em S8
+
+        print("função para pedir cobertor e luvas")
+        pass
+
+    def test_order_2_ice_creams(self):
+        # Adicionar em S8
+
+        print("função para pedir 2 sorvetes")
+        pass
+
+    def test_car_search_model_appears(self):
+       # Adicionar em S8
+
+       print("função para procurar modelo do carro")
+       pass
+
+
